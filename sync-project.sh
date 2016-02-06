8476e899a5721d9c6950da222d52eb0f8a2636af
Alexander Reitzel <funtimecoding@gmail.com>
2016-02-06 17:35:32 +0100

    more verbose error messages, detect project names like CSharpSkeleton, put project path in quotes

--- sync-project.sh
+++ sync-project.sh
@@ -9,15 +9,15 @@ if [ "${TARGET_PROJECT}" = "" ]; then
 fi
 
 if [ ! -d "${TARGET_PROJECT}" ]; then
-    echo "Target directory does not exist."
+    echo "Target directory ${TARGET_PROJECT} does not exist."
 
     exit 1
 fi
 
-CAMEL=$(head -n1 "${TARGET_PROJECT}"/README.md | awk '{ print $2 }' | grep -E '^([A-Z][a-z0-9]+){2,}$') || CAMEL=""
+CAMEL=$(head -n1 "${TARGET_PROJECT}"/README.md | awk '{ print $2 }' | grep -E '^([A-Z]+[a-z0-9]*){2,}$') || CAMEL=""
 
 if [ "${CAMEL}" = "" ]; then
-    echo "Could not determine the target projects name."
+    echo "Could not determine the projects name in ${TARGET_PROJECT}."
 
     exit 1
 fi
@@ -40,5 +40,5 @@ cd "${TARGET_PROJECT}" || exit 1
 # TODO: Uncomment the next line after initializing a concrete skeleton project.
 #rm init-project.sh sync-project.sh
 # shellcheck disable=SC2016
-${FIND} . -type f -regextype posix-extended ! -regex '^.*/(\.git|\.idea)/.*$' -exec sh -c '${1} -i -e "s/SkeletonBase/${2}/g" -e "s/skeleton-base/${3}/g" ${4}' '_' "${SED}" "${CAMEL}" "${DASH}" '{}' \;
+${FIND} . -type f -regextype posix-extended ! -regex '^.*/(\.git|\.idea)/.*$' -exec sh -c '${1} -i -e "s/SkeletonBase/${2}/g" -e "s/skeleton-base/${3}/g" "${4}"' '_' "${SED}" "${CAMEL}" "${DASH}" '{}' \;
 echo "Done. Files were copied to ${TARGET_PROJECT} and modified. Review those changes."
